b98253b7ebecefbdda2e49efe731851d6171952a
dummy <dummy@wifi-roaming-128-4-93-231.host.udel.edu>
2017-05-12 12:16:05 -0400

    tad-header

--- newatlas/tad_header.php
+++ newatlas/tad_header.php
@@ -1,24 +1,54 @@
-<!DOCTYPE php>
-<!-- Main Page -->
-<php lang='en'>
-<head>
-	<meta charset="UTF-8" />
-	<link rel="STYLESHEET" type="text/css" href="stylefile.css">
-	<link rel="icon" type="image/ico" href="images/icon.png"/>
-	<div>
-		<!--heading-->
-		<center>
-			<h1>TransAtlasDB</h1>
-		</center>
-	</div>
-	<div id="menu">
-		<!--menu-->
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="dataimport.php">Data Import</a></li>
-			<li><a href="dataexport.php">Data Export</a></li>
-		</ul>
-	</div>
-</head>
-<body>
-	<div id="bodys">
+<?php
+function theader() {
+echo '
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TransAtlasDB</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        
+        <!-- Styles -->
+        <link rel="STYLESHEET" type="text/css" href="stylesheet.css">
+        
+        <div class="title sub-md">
+            <a href="index.php">TransAtlasDB</a>
+        </div>
+        <center>
+            <div class="links">
+                <a href="about.php">About</a>
+                <a href="dataimport.php">Data Import</a>
+                <a href="metadata.php">MetaData</a>
+                <a href="genes.php">Genes Expression</a>
+                <a href="variants.php">Variants</a>
+                <a href="https://modupeore.github.com/TransAtlasDB" target="_blank">GitHub</a>
+            </div>
+        </center>
+    </head>
+';
+}
+?>
+
+<?php
+function tmetadata() {
+    theader();
+    echo "<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />";
+    echo "<title>Metadata</title>";
+    echo '<script type="text/javascript" src="/code.jquery.com/jquery-1.8.3.js"></script>';
+    echo "<style type= 'text/css'></style>";
+?>
+    <script type="text/javascript">
+    function selectAll(source) {
+        checkboxes = document.getElementsByName('meta_data[]');
+        for(var i in checkboxes)
+        checkboxes[i].checked = source.checked;
+    }
+<?PHP
+    echo "</script></style>";
+}
+?>
