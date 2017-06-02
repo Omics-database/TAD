305d424563e5225f3ae184e369a0a390eef9b5af
dummy <dummy@dummys-MBP.hsd1.de.comcast.net>
2017-06-02 15:45:51 -0400

    added README

--- web/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  define('DB_SERVER', 'localhost');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', 'rainbow');
-  define('DB_DATABASE', 'transatlasdb');
-  $db_conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-  $basepath = '/Users/dummy/WebDocs/TAD';
-?>
